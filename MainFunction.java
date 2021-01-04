e57ba87018e3c832595b3b1debf97f3ed1467fd8
mashiat0808 <59837488+mashiat0808@users.noreply.github.com>
2021-01-04 16:30:23 +0600

    Update and rename MainFunc.java to MainFunction.java

---
+++ MainFunction.java
@@ -0,0 +1,10 @@
+import java.utility.*;
+public class MainFunction {
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        BankAccount account1 = new BankAccount();
+
+        System.out.println("Mashiat Amin Farin" +account1.getBalance(15000));
+}
+}
